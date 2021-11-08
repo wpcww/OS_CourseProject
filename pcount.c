3c22fd8b1c09784569695d7bf46f5375c730e122
wpcww <chront4n@gmail.com>
2021-11-09 00:43:31 +0800

    update counter structure, add print function

--- pcount.c
+++ pcount.c
@@ -23,40 +23,52 @@
 //#include <sys/sysinfo.h>
 #include <unistd.h>
 
-typedef struct dataStore {
-    int a_count;    //count of char "a"
+
+struct dataStore {
     int index;      //current index on the input string
     char *content;  //pointer to the input string
+    int resultArr[26];
 } dataStore;
 
-void *test1(void * arg){
-    dataStore *datL = (dataStore *)arg;
+void *test1(void *arg){
+    struct dataStore *datL = (struct dataStore *)arg;
+    char c = 97; //ascii a = 97
+
     while(datL->index < strlen(datL->content)){
         //sleep(1);
         char *cp1 = &datL->content[datL->index];
-        if(strncmp(cp1,"a",1) == 0){
-            datL->a_count += 1;
+        if(strncmp(cp1, &c, 1) == 0){
+            datL->resultArr[0] += 1;
         }
         datL->index += 1;
     }
     pthread_exit(NULL);
 }
 
-int main(){
+void printResult(int arr[26]){
+    for(int i = 0; i < 25; i++){
+        if(arr[i] > 0){
+            printf("%c %d\n", 97 + i, arr[i]);
+        }
+    }
+}
+
+int main(int argc, char *argv[]){
     pthread_t t1;
     pthread_t t2;
-    dataStore dat;
+    struct dataStore dat;
     char input[] = "aaxasaaoirgha[oi"; //file input here
 
-    dat.a_count = 0;
     dat.index = 0;
     dat.content = input;
+    memset(dat.resultArr, 0, sizeof(dat.resultArr));
 
     pthread_create(&t1, NULL, test1, (void *)&dat);
     pthread_create(&t2, NULL, test1, (void *)&dat);
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
-    printf("a count: %d\n", dat.a_count);
+    //printf("a count: %d\n", dat.resultArr[0]);
+    printResult(dat.resultArr);
 
 
     return 0;
