82091bce106f13022e3f413b62eb4ab2a44d4eb3
wpcww <chront4n@gmail.com>
2021-11-26 02:36:46 +0800

    work up to test 8

--- pcount.c
+++ pcount.c
@@ -4,9 +4,9 @@
  *
  * Group No.: 10 (Join a project group in Canvas)
  * First member's full name: Marcus Owen LEE
- * First member's email address: 
+ * First member's email address: marcuslee6-c@my.cityu.edu.hk
  * Second member's full name: Sze Man FUNG
- * Second member's email address: 
+ * Second member's email address: szfung9-c@my.cityu.edu.hk
  * Third member's full name: Wai Pong CHEUNG
  * Third member's email address: wpcheung33-c@cityu.edu.hk
  */
@@ -25,62 +25,131 @@
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-typedef struct sharedMemory {   //create a type of sharedMemory for splitting file 
-    int counter;                // number of chars needed to be counted
-    int charCount[26];          // to store the count of a~z in the specific part of file
-    int startIndex;             // to store the location of where a specific thread started to count
-    char *content;              // pointer to buffer of file content at line #104
+typedef struct sharedMemory {
+    int counter;        //Record sorted number of chars
+    int charCount[26];
+    int startIndex;
+    char *content;
 } sharedMemory;
+
+typedef struct singleFileMemory {
+    char *filename; //*sorted[x]
+    int fileCharCount[26];
+    int aThread;    //Number of threads that will be used
+} singleFileMemory;
 
 void *thread(void *arg){
     sharedMemory *datL = (sharedMemory *)arg;
-    int indL = datL->startIndex;    //begin of the partition
-    int endL = datL->counter;       //end of the partition
+    int indL = datL->startIndex;
+    int endL = datL->counter;
     for(int i = 0; i < endL; i++){
-        datL->charCount[datL->content[indL + i] - 97] += 1; //Convert the character to the index of the counting list, increment the count value
+        if(datL->content[indL + i] - 97 >= 0 && datL->content[indL + i] - 97 <= 25){
+            datL->charCount[datL->content[indL + i] - 97] += 1; //Convert the character to the index of the counting list, increment the count value
+        }
     }
-    pthread_exit(NULL);             //terminate the thread
+
+    pthread_exit(NULL);
 }
 
-void printResult(int arr[26]){  //void print the characters of occurences in the file
+void printResult(int arr[26]){
     for(int i = 0; i < 26; i++){
         if(arr[i] > 0){
-            printf("%c %d\n", 97 + i, arr[i]);  //print the characters that it occurs in file in alphabetical  order, arr[i] means number of occurences
+            printf("%c %d\n", 97 + i, arr[i]);
         }
     }
 }
 
-sharedMemory datInit(int j, int byteCount, int aThread, char *buffer){
+sharedMemory datInit(int j, int byteCount, int divisor, char *buffer){
     sharedMemory dat;
     dat.content = buffer;
-    dat.startIndex = 0 + j * (byteCount - byteCount%aThread)/aThread;
-    if(j == aThread){   //check if the partition is the last partition of file, to determine the partition size 
-        dat.counter = byteCount % aThread;
+    dat.startIndex = 0 + j * (byteCount - byteCount%divisor)/divisor;
+    if(j == divisor){
+        dat.counter = byteCount % divisor;
     }else{
-        dat.counter = (byteCount - byteCount % aThread)/aThread;
+        dat.counter = (byteCount - byteCount % divisor)/divisor;
     }
     memset(dat.charCount, 0, sizeof(dat.charCount)); //Initialize with all 0, otherwise random number will appear
     return dat;
 }
 
-int main(int argc, char *argv[]){       //argc: how many files, argv: the array of arguments
-    char *sorted[argc-1];               //to store a sorted order of file names
-    char fileCount = argc - 1;
-    if(fileCount == 0){                 //for case of no file, output required text
+void *singleFileThread(void *arg){
+    singleFileMemory *datL = (singleFileMemory *)arg;
+    char *filename = datL->filename;
+    int availableThread = datL->aThread;
+    int divisor = availableThread - 1;
+    char *buffer;
+    long byteCount;
+    FILE *f;
+    f = fopen(filename, "r");
+    fseek(f, 0L, SEEK_END);
+    byteCount = ftell(f);
+    fseek(f, 0L, SEEK_SET);
+    buffer = (char*)calloc(byteCount, sizeof(char));
+    byteCount = fread(buffer, sizeof(char), byteCount, f); //fread return value needs to be stored and used
+    fclose(f);
+
+    pthread_t *threadStack;
+    threadStack = (pthread_t *)malloc(availableThread * sizeof(pthread_t));
+    sharedMemory datStack[availableThread];
+    for(int j = 0; j < availableThread; j++){
+        datStack[j] = datInit(j, byteCount, divisor, buffer);
+        pthread_create(&threadStack[j], NULL, thread, (void *)&datStack[j]);
+    }
+    for (int j = 0; j < availableThread; j++){
+        pthread_join(threadStack[j], NULL);
+        for(int k = 0; k < 26; k++){
+            datL->fileCharCount[k] += datStack[j].charCount[k];
+        }
+    }
+    free(buffer);
+    free(threadStack); 
+
+
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]){
+    char argvCopy[20][40];  //Create a copy of argv
+    struct stat dirOrFile;  //stat object to for file or directory check
+    struct dirent *d_temp;  //temp char for storing prefix of the file
+    int fileCount = 0;
+    for(int i = 1; i < argc; i++){
+        stat(argv[i], &dirOrFile);  //Store the stat information of input to the structure
+        if(S_ISREG(dirOrFile.st_mode) == 1){ //1 is a file, 0 is a directory
+            strcpy(argvCopy[i - 1], argv[i]); //Save the filename to the copy list
+            fileCount += 1;
+        }else{
+            DIR *dir = opendir(argv[i]);    //If it is a dir, open it
+            int j = i - 1;
+            readdir(dir); //Skip current dir
+            readdir(dir); //Skip previous dir
+            while((d_temp = readdir(dir)) != NULL){
+                strcpy(argvCopy[j], argv[i]);   //Add file prefix
+                strcat(argvCopy[j], "/");   //Add directory symbol
+                strcat(argvCopy[j], d_temp->d_name);    //Add filename
+                fileCount += 1;
+                j++;
+            }
+            closedir(dir);
+        }
+    }
+
+    char *sorted[fileCount];
+    if(fileCount == 0){
         printf("pzip: file1 [file2 ...]\n");
         return 1;
-    }else if(fileCount == 1){           //Single file, no need to do sorting
-        sorted[0] = argv[1];
-    }else if(fileCount > 1){            //method to sort by file name
-        for(int i = 0; i < argc - 1; i++){
-            sorted[i] = argv[i + 1];    //putting file names into sorted [] for sorting
+    }else if(fileCount == 1){
+        sorted[0] = argvCopy[0];
+    }else if(fileCount > 1){
+        for(int i = 0; i < fileCount; i++){
+            sorted[i] = argvCopy[i];
         }
-        char *cmp;                      //act as a temporary variable that store the character of old sorted position 
+        char *cmp;
         for(int i = 0;i <fileCount;i++) 
         {
             for(int j = i + 1;j < fileCount;j++)
             {           
-                if(strcmp(sorted[i], sorted[j]) > 0)    //sort the file name positions if the latter one is smaller than former one
+                if(strcmp(sorted[i], sorted[j]) > 0)
                 {   
                     cmp = sorted[i];
                     sorted[i] = sorted[j];   
@@ -90,41 +159,21 @@ int main(int argc, char *argv[]){       //argc: how many files, argv: the array 
         }
     }
 
-    int availableThread = 3;            //Set the number of threads that will be used
-    int aThread = availableThread - 1;  //base on availableThread
-    char *buffer;                       //file content
-    long byteCount;                     //count of characters in one file
-    for (int i = 0; i < fileCount; i++){
-        int mainCountList[26];          //26 characters of a~z
-        memset(mainCountList, 0, sizeof(mainCountList));
-        FILE *f;
-        f = fopen(sorted[i], "r");
-        fseek(f, 0L, SEEK_END);
-        byteCount = ftell(f);
-        fseek(f, 0L, SEEK_SET);
-        buffer = (char*)calloc(byteCount, sizeof(char));        //allocate memory for file content
-        byteCount = fread(buffer, sizeof(char), byteCount, f);  //fread return value needs to be stored and used, required during compilation
-        fclose(f);
-
-        pthread_t *threadStack; // pointer to threads that will be created
-        threadStack = (pthread_t *)malloc(availableThread * sizeof(pthread_t)); //allocate memory to the threads base on value of availableThread
-        sharedMemory datStack[availableThread]; //prepare array of shared memory for pthread_create method base on value of availableThread
-        for(int j = 0; j < availableThread; j++){
-            datStack[j] = datInit(j, byteCount, aThread, buffer);   // Initiate shared memory for thread creation
-            pthread_create(&threadStack[j], NULL, thread, (void *)&datStack[j]);    //Create thread
-        }
-        for (int j = 0; j < availableThread; j++){  //to combine all of the counts from different threads
-            pthread_join(threadStack[j], NULL); //suspend execution of thread until the target threads terminate
-            for(int k = 0; k < 26; k++){
-                mainCountList[k] += datStack[j].charCount[k];
+    pthread_t *fileThreadStack;
+    fileThreadStack=(pthread_t *)malloc(fileCount * sizeof(pthread_t ));    //Initialize thread stack for file multithreading
+    singleFileMemory fileDataStack[fileCount];  //Initialize information structure required by the thread
+            for (int i = 0; i < fileCount; i++){
+                fileDataStack[i].filename = sorted[i];  //Assign filename
+                fileDataStack[i].aThread = 7;   //Assign number of threads that would be used by a thread
+                memset(fileDataStack[i].fileCharCount, 0, sizeof(fileDataStack[i].fileCharCount));  //Initialze list with all 0
+                pthread_create(&fileThreadStack[i], NULL, singleFileThread, (void *)&fileDataStack[i]);
             }
-        }
-        free(buffer);   //free the memory 
-        free(threadStack);  //free the memory 
-        printf("%s\n", sorted[i]);  //print file name
-        printResult(mainCountList); //print result line #49
-    }
+            for (int i = 0; i < fileCount; i++){
+                pthread_join(fileThreadStack[i], NULL);
+                printf("%s\n", fileDataStack[i].filename);  //Print file name
+                printResult(fileDataStack[i].fileCharCount);    //Print result
+            }
 
-
+    free(fileThreadStack); 
     return 0;
 }
